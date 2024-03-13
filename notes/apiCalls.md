e469202e529cca6b26be3cc1ac125f15a3d5c727
estradagabe1996 <estradagabe1996@gmail.com>
2024-03-13 14:47:38 -0400

    EOD 3_13

---
+++ notes/apiCalls.md
@@ -0,0 +1,22 @@
+how api calls work -
+
+every user interacts with interfaces using a GUI
+
+clients- any device that calls on APIs 
+ex. laptops, phones, washing machines 
+
+java script object notation "JSON"
+
+Modern API
+CRUD API 
+Create - POST - add a new resource to the server
+Read - GET - get data back from the sercer 
+Update - PUT - changing the attributes of an existing resource 
+Delete - DELETE - delete a resource from the server
+
+STATUS CODES 
+200: OK, request was received and granted 
+401: Unauthenticated - you are not someone we recongnize 
+403: Forbidden - we have verified that you are who you say you are, but you do not have permission to preform the action
+404: Not Found -- this could be user error and just typing in the wrong url, or It could mean that the site doesn't exist any more like vine or myspace
+
---
+++ notes/forms.js
@@ -0,0 +1,46 @@
+function getEmail() {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  
+  console.log("Email: ", email.value);
+  console.log("Password: ", password.value);
+  userLogins.push(email, password);
+}
+
+let userLogins = [
+  {
+      "name": "John Doe",
+      "email": "john.doe@example.com",
+      "password": "password1",
+      "address": "123 Main St",
+      "zip_code": "12345",
+      "phone_number": "123-456-7890"
+  },
+  {
+      "name": "Jane Smith",
+      "email": "jane.smith@example.com",
+      "password": "password2",
+      "address": "456 Elm St",
+      "zip_code": "23456",
+      "phone_number": "234-567-8901"
+  },
+  {
+      "name": "Michael Johnson",
+      "email": "michael.johnson@example.com",
+      "password": "password3",
+      "address": "789 Oak St",
+      "zip_code": "34567",
+      "phone_number": "345-678-9012"
+  },
+  // Continue adding other users here...
+]
+
+
+function checkPassword() {
+    for (i = 0 ; i < userLogins.length; i++){
+      if(getEmail.match(userLogins[i].password)) {
+
+      }
+    }
+
+}
